9af46ad299021dc5b48a3cc2c6205a10ff6d8b0d
Joseph Martinez <insertepala@gmail.com>
2024-06-25 17:12:42 -0500

    First commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,14 @@
+import cross from '../assets/cross.svg'
+
+function Task({ done = false, id, task, handleDelete, handleDone}) {
+    return (
+        <div onClick={handleDone} id={id} className={`task ${done ? 'task--done' : ''}`}>
+            <p>{task}</p>
+            <button onClick={handleDelete}>
+                <img src={cross} alt="Close icon" />
+            </button>
+        </div>
+    )
+}
+
+export default Task
